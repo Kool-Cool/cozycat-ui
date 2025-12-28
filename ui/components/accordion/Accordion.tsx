0cb2148ffa95d8a02612584fce3fa27ab7ac4964
Kool-Cool <omkarjadhav0507@gmail.com>
2025-12-28 20:36:31 +0530

    Update Accordion.tsx

--- ui/components/accordion/Accordion.tsx
+++ ui/components/accordion/Accordion.tsx
@@ -37,7 +37,7 @@ export const Accordion: FC<AccordionProps> = ({
           </>
         )}
 
-        <CatIcon className="w-5 h-5 mr-2 text-red-700" /> 
+        {/* <CatIcon className="w-5 h-5 mr-2 text-red-700" />  */}
         {title}
       </button>
 
